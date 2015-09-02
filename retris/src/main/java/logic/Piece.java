5bbc393e0cf80af1c37ea27ef4ec5038f0090504
Rochet2 <tqbattlenet@gmail.com>
2015-09-02 22:50:54 +0300

    kansioinnin korjaus ja työn aloitus
    - javalabran dokumentoinnissa puutteita? Ohjeiden jälkeen main vaikea
      tehdä ellei tiedä miten tai kopioi muusta projektista.
    - luokkia ja metodeja.

---
+++ retris/src/main/java/logic/Piece.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2015 rochet2_2
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package logic;
+
+/**
+ *
+ * @author rochet2_2
+ */
+public class Piece {
+    private Position position;
+    private int color;
+
+    /**
+     * @return the position
+     */
+    public Position getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    /**
+     * @return the color
+     */
+    public int getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(int color) {
+        this.color = color;
+    }
+    
+}
